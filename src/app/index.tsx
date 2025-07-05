3f3a104ed68fea8b352f91d27d3e4d59fe3cb5e7
Keventon <kevertonguimaraes19@gmail.com>
2025-07-05 15:27:46 -0300

    feito componente de lista

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,6 @@
 import { HomeHeader } from "@/components/HomeHeader";
+import { List } from "@/components/List";
+import { Target } from "@/components/Target";
 import { View } from "react-native";
 
 const summary = {
@@ -13,10 +15,43 @@ const summary = {
   },
 };
 
+const targets = [
+  {
+    id: "1",
+    name: "Comprar uma cadeira ergonômica",
+    percentage: "75%",
+    current: "900",
+    target: "1.200",
+  },
+  {
+    id: "2",
+    name: "Apple Watch Series 7",
+    percentage: "50%",
+    current: "1.000",
+    target: "2.000",
+  },
+  {
+    id: "3",
+    name: "Comprar um mouse da Logitech",
+    percentage: "25%",
+    current: "100",
+    target: "500",
+  },
+];
+
 export default function Index() {
   return (
     <View style={{ flex: 1 }}>
       <HomeHeader data={summary} />
+
+      <List
+        title="Metas"
+        data={targets}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Target data={item} />}
+        emptyMessage="Nenhuma meta cadastrada. Toque em nova meta para criar."
+        containerStyle={{ paddingHorizontal: 24 }}
+      />
     </View>
   );
 }
